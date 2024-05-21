06b3df83f6096c4fc53a8bbea54beec0fdc7d060
Necron <127852628+Necron329@users.noreply.github.com>
2024-05-21 12:04:45 +0000

    kommit

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 function doThings() {
-    let name = document.querySelector("#name").value;
-    let surname = document.querySelector("#surname").value;
-    let pesel = document.querySelector("#pesel").value;
+    // let name = document.querySelector("#name").value;
+    // let surname = document.querySelector("#surname").value;
+    // let pesel = document.querySelector("#pesel").value;
+
+    let name = "jan";
+    let surname = "kowalski";
+    let pesel = "12345678910";
 
     var mysql = require("mysql")
 
     var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'kurs_node'
+        host: '127.0.0.1',
+        user: 'mariadb',
+        password: 'mariadb',
+        database: 'mariadb'
     })
 
     connection.query("INSERT INTO ludzie VALUES (NULL, " + name + ", " + surname + ", " + pesel + ");")
